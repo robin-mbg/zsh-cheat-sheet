ae671282199058cc59acc931b58e3ec287103404
Robin Meiseberg <rmeiseberg@gmail.com>
2019-10-22 19:37:43 +0200

    Pivot direction to creating a self-managed cheat sheet of program tags.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # zsh-cheat-sheet
 
-This zsh plugin adds a `cc` function that allows you to easily filter and view your most frequently used terminal commands - and easily find that one command that saved you a few months back.
+zsh plugin that adds a `cs`/`cheat_sheet` function that lists all commands in the history that have been tagged with a comment.
 
+Use as follows:
+
+1. When you encounter a command you'd like to keep, simply execute it with a comment attached.
+
+```sh
+ls -a #List all files
+```
+
+2. Next time you want to execute something that has been tagged, just use `cc` and then use the fuzzy matching provided.
+
+### Dependencies
+- fzf
+- awk
--- zsh-cheat-sheet.plugin.zsh
+++ zsh-cheat-sheet.plugin.zsh
@@ -1,10 +1,11 @@
-alias cc="cheat_sheet"
+alias cs="cheat_sheet"
 
-function cheat_sheet() {
+function cheat_sheet(){
     if [ -z $1 ]
     then
-        $(history | awk -F" " '{ print $2,$3,$4,$5 }' | sort  | uniq --count | sort --numeric-sort --reverse | head -50 | fzf | awk -F" " '{ print $2,$3,$4,$5 }')
+        $(noglob history | awk '{$1="";print substr($0,2)}' | sed '/^[[:blank:]]*#/d;' | uniq | grep " #" | grep -v "history " | fzf | sed 's/#.*//')
     else
-        $(history | grep "$1" | awk -F" " '{ print $2,$3,$4,$5 }' | sort  | uniq --count | sort --numeric-sort --reverse | head -50 | fzf | awk -F" " '{ print $2,$3,$4,$5 }')
+        echo "Arguments not yet supported."
+        cheat_sheet
     fi
 }
